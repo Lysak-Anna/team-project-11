49bbb05d73c29d2340a8de2feeab6b4e6982aa05
Lysak-Anna <112387164+Lysak-Anna@users.noreply.github.com>
2022-10-19 20:10:59 +0000

    Deploying to gh-pages from @ Lysak-Anna/team-project-11@c4e97830b6632c42b473727f84f9ac0bfde1a34f 🚀

---
+++ index.bc5ee55e.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".slider-reviews",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(e,i){return'<span class="'+i+'">0'+(e+1)+"</span>"}}});
+//# sourceMappingURL=index.bc5ee55e.js.map
