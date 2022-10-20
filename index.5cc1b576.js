1f53db0737554d6cd5d1110eaa13f587567318c2
Lysak-Anna <112387164+Lysak-Anna@users.noreply.github.com>
2022-10-20 19:57:52 +0000

    Deploying to gh-pages from @ Lysak-Anna/team-project-11@d239485aa916e589f3dcf833276863fac3e586ee 🚀

---
+++ index.5cc1b576.js
@@ -0,0 +1,2 @@
+var openModalBtn=document.querySelector(".order-list__button"),backdrop=document.querySelector(".backdrop-stores"),modal=document.querySelector(".modal-stores"),closeModalBtn=document.querySelector(".modal-stores__close");openModalBtn.addEventListener("click",(function(){backdrop.classList.remove("is-hidden")})),closeModalBtn.addEventListener("click",(function(){backdrop.classList.add("is-hidden")}));
+//# sourceMappingURL=index.5cc1b576.js.map
