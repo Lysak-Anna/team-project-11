1a3a7c2f2c6c9c98d73efa89281039488fe8b45e
Lysak-Anna <112387164+Lysak-Anna@users.noreply.github.com>
2022-10-20 13:18:58 +0000

    Deploying to gh-pages from @ Lysak-Anna/team-project-11@8a9315af12cc5217bf4422f6972bb92b5692ca76 🚀

---
+++ index.46481102.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("scroll-lock")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.46481102.js.map
