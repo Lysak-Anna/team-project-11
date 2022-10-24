57456c87abf2dcad20ba52ca6f42a69c1d0318db
efirnet <efirnet@gmail.com>
2022-10-25 00:48:46 +0300

    add modal-payment,add  btn order, fix btn

---
+++ src/js/modal-order.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[order-modal-open]"),
+    closeModalBtn: document.querySelector("[order-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('scroll-lock');
+  }
+})();
