55f8b42214394619c2fd93470e6f8da14e5ca2ca
efirnet <efirnet@gmail.com>
2023-07-08 10:16:41 +0300

    refactor code in index files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+import Swiper from 'swiper';
 
-   
- 
-  var swiper = new Swiper(".slider-reviews", {
-         pagination: {
-           el: ".swiper-pagination",
-           clickable: true,
-           renderBullet: function (index, className) {
-             return '<span class="' + className + '">' + "0" + (index + 1) + "</span>";
-           },
-         },
-   });
-    
-  
+import './js/content.js';
+import './js/menu.js';
+import './js/modalBose.js';
+import './js/modalOrder.js';
+
+const swiper = new Swiper('.slider-reviews', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
+    },
+  },
+});
